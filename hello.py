a983d1fa1d3a56d1d6d5c6ed60398d3b89444a9d
Jeff Sloyer <jbsloyer@us.ibm.com>
2015-03-21 13:19:43 -0400

    tweaked to run locally

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
 """Cloud Foundry test"""
 from flask import Flask
 import os
- 
+
 app = Flask(__name__)
- 
-port = int(os.getenv("VCAP_APP_PORT"))
- 
+
+if os.getenv("VCAP_APP_PORT"):
+    port = int(os.getenv("VCAP_APP_PORT"))
+else:
+    port = 8080
+
 @app.route('/')
 def hello_world():
     return 'Hello World! I am running on port ' + str(port)
- 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
