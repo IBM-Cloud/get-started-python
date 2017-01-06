6cdf63b62045dd719cf1d380b8dd755032378957
Ram Vennam <rvennam@users.noreply.github.com>
2017-01-06 11:30:43 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ cf_deployment_tracker.track()
 
 app = Flask(__name__)
 
-# On Bluemix, get the port number from the environment variable VCAP_APP_PORT
+# On Bluemix, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8080
 port = int(os.getenv('PORT', 8080))
 
